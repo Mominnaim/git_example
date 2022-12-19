6febe3f8bd30579040b2085e258a8b70ba5b1cbd
naim <MominNaim11@gmail.com>
2022-12-18 20:43:52 -0600

    modified ex25

--- ex25.py
+++ ex25.py
@@ -1,6 +1,6 @@
 def break_words(stuff):
     """this function will break up words for us"""
-    words = stuff.split('')
+    words = stuff.split(' ')
     return words
 
 def sort_words(words):
@@ -9,24 +9,29 @@ def sort_words(words):
 
 def print_first_word(words):
     """Prints the first word after popping it off. """
+    word = words.pop(0)
+    print(word)
+
+def print_last_word(words):
+    """Prints the last word after popping it off. """
     word = words.pop(-1)
     print(word)
 
 def sort_sentence(sentence):
     """Takes in a full sentence and returns the sorted words."""
-    word = words.pop(-1)
-    print(words)
+    words = break_words(sentence)
+    return sort_words(words)
 
 def print_first_and_last(sentence):
     """Prints the first and last words of the sentence."""
     words = break_words(sentence)
     print_first_word(words)
-    print_last_words(words)
+    print_last_word(words)
 
 def print_first_and_last_sorted(sentence):
     """Sorts the word then prints the first and last one."""
-    words = sort_sentance(sentence)
-    print_the_word(words)
+    words = sort_sentence(sentence)
+    print_first_word(words)
     print_last_word(words)
 
     
