966cd3be007b0c7077b9627a966b24525e13b106
naim <MominNaim11@gmail.com>
2022-12-27 20:39:00 -0600

    Pushing two files, that were long as hell

---
+++ ex35_branches_functions.py
@@ -0,0 +1,69 @@
+from sys import exit
+
+# This will be the starting room and from here doors will open up.
+
+def black():
+    print("You have arrived in the black room")
+    print("The answer to this question will lead you to the next door so pick wisely.")
+    print("There is a train coming and has a blue path and a red path.")
+    print("On the blue path there is a 5 people attached to the trail.")
+    print("On the red path there is a pregnant lady attached to the trail.")
+    print("You have the lever to decide who will live and who will die.")
+
+    print("What path are you picking for the train to through")
+    pick_path = input()
+
+    if pick_path.lower() == "red":
+        red()
+    elif pick_path.lower() == "blue":
+        blue()
+    else:
+        exit(0)
+
+    
+def red():
+    print("So you decided to kill the pregnant lady.")
+    print("You should wonder why you chose that not the blue path")
+
+    print("Next question: If you had to take one test to save your friends life which test would it be")
+    print("you have two options -> MATH or SCIENCE")
+
+    pick_test = input(" > ")
+
+    if pick_test.lower() == "math":
+        MATH()
+    elif pick_test.lower() == "science":
+        SCIENCE()
+    else:
+        exit(0)
+
+def blue():
+
+
+    print("You decided to kill 5 people, how do you feel, murdered!!")
+    print("Just kidding, this is just an exercise")
+    print("But heres your next question -> pick 1 or 2")
+
+    pick_Number = input()
+
+    if pick_Number.lower() == "1":
+        print("Your favorite number is 1.")
+    elif pick_Number.lower() == "2":
+        print("Your favorite number is 2.")
+    else:
+        print("BREAK THE RULES MAYNEE")
+        exit(0)
+
+def MATH():
+    print("You love number dont you!!")
+
+def SCIENCE():  
+    print("You love concepts dont you!!")
+
+black()
+
+
+
+
+
+
